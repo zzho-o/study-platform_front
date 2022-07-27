8e87ae8775bdc46df666f25fe20ea63339ef15fb
zzho-o <rkdwjsgh0@naver.com>
2022-07-28 04:22:03 +0900

    'UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Button from "./Button";
+import AppBar from "./AppBar";
+import Box from "@mui/material/Box";
+import InfoIcon from "@mui/icons-material/Info";
+import Index from "./Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <header className="App-header">
+        <Button variant="outlined">Create</Button>
+        <AppBar>menu</AppBar>
+        <h2> {name} !</h2>
+      </header> */}
+      <Index />
     </div>
   );
 }
