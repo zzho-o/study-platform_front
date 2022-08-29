a7bab08437b884dd666b2d9b05cee9873d8fa236
zzho-o <rkdwjsgh0@naver.com>
2022-08-29 17:22:25 +0900

    'feature'

--- src/App.js
+++ src/App.js
@@ -11,27 +11,21 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate
+  Navigate,
 } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/chat/:id" element={<Chat/>} />
-      {/* <header className="App-header">
+        <Route path="/chat/:id" element={<Chat />} />
+        {/* <header className="App-header">
         <Button variant="outlined">Create</Button>
         <AppBar>menu</AppBar>
         <h2> {name} !</h2>
       </header> */}
-<<<<<<< HEAD
-      <Home />
-      {/* <Chat /> */}
-=======
-      {/* <Home /> */}
->>>>>>> 1ad7237fd9b5e0f18ae653cee148048103a12320
-      {/* <Mypage /> */}
-    </Routes>
+        <Home />
+      </Routes>
     </Router>
   );
 }
