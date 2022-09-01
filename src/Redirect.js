25917df0b4657d6c0f3f8098a543ec3a773b39d1
zzho-o <rkdwjsgh0@naver.com>
2022-09-02 01:12:00 +0900

    'feature'

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,9 +1,25 @@
 import * as React from "react";
+const axios = require("axios");
 
 function Redirect() {
   const code = new URL(window.location.href);
   const code_params = code.searchParams.get("code");
-  console.log(code_params);
+  const get_info = async () => {
+    try {
+      return await axios.post(
+        "https://jadu-study-flatform.shop/api/oauth/kakao"
+      );
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const print_info = async () => {
+    const info = await get_info();
+
+    console.log(info);
+  };
+  // print_info()
+  print_info();
   //   React.useEffect(async () => {});
   return <div></div>;
 }
