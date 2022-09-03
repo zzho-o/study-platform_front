21834d73e0d2daa302ec50d8a54e9c1896e0e832
ohjinseo <62508156+ohjinseo@users.noreply.github.com>
2022-09-03 16:04:29 +0900

    feat : 채팅 클라이언트 api

--- src/Chat/ChatBox.jsx
+++ src/Chat/ChatBox.jsx
@@ -1,11 +1,14 @@
-import React, { useRef, useEffect } from 'react'
+import React, { useRef, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
 import Message from './Message';
 import AttachFileOutlinedIcon from '@mui/icons-material/AttachFileOutlined';
 
 import SockJS from "sockjs-client";
 import Stomp from "stompjs";
 import styled from 'styled-components';
+import { getChatMessage } from '../redux/slices/chatSlice';
+import axios from 'axios';
 
 const Container = styled.div`
     flex: 6;
@@ -62,50 +65,94 @@ const ChatBox = () => {
     const ws = useRef();
     const params = useParams();
     const roomId = params.id;
+    const dispatch = useDispatch();
+    const [otherUserInfo, setOtherUserInfo] = useState({});
+    const [text, setText] = useState("");
+
+    const messages = useSelector((state) => state.chatReducer.messages.result);
+
+    const config = {
+        headers: {
+            "X-ACCESS-TOKEN": window.localStorage.getItem("token")
+        }
+    }
+
 
     // 소켓 연결
     useEffect(() => {
-        let sock = new SockJS(`${process.env.REACT_APP_BASE_URL}/stomp/chat`);
+        let sock = new SockJS(`http://localhost:9000/stomp/chat`);
         let client = Stomp.over(sock);
         ws.current = client;
-
-        console.log(client);
 
         ws.current.connect({}, () => {
             ws.current.subscribe(`/sub/chat/room/${roomId}`, (res) => {
                 const message = JSON.parse(res.body);
-                
+                console.log(message);
             });
         })
     }, [roomId]);
+
+    useEffect(() => {
+        dispatch(getChatMessage({roomId}));
+    }, []);
+
+    useEffect(() => {
+        axios.get(`/api/chat-rooms/otherUserInfo/${roomId}`, config)
+            .then((response) => {
+                setOtherUserInfo(response.data.result.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    useEffect(() => {
+        scrollRef.current.scrollIntoView({ block: "end" });
+    }, [messages]);
+
+    // 메시지 발신
+    const send = async () => {
+        try {
+            // send할 데이터
+            const message = {
+                roomId: roomId,
+                message: text,
+                otherUserIds: [1], // 메시지 받는 상대방
+                type:0,
+                userId:5 // 임시
+            };
+
+            if (text === "") {
+                return;
+            }
+            // send message
+            ws.current.send("/pub/chat/message", {}, JSON.stringify(message));
+            setText("");
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    //setTimeout(() => ws.current.send("/pub/chat/message", {}, JSON.stringify({"roomId":1, "otherUserIds":[5], "userId":1, "type":0, "message":"아녕하세요!!ㄸ"})), 500); 
+
+    if (messages === null) {
+        return;
+    }
 
   return (
     <Container>
     <ChatBoxWrapper>
         <ChatBoxTop>
             <div ref={scrollRef}>
-                <Message me={false}/>
-                <Message me={true}/>
-                <Message me={false}/>
-                <Message me={true}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
-                <Message me={false}/>
+                {messages !== undefined && messages.data.map((message, idx) => (
+                    <Message createdAt={message.createdAt} key={idx} message={message.message} me={message.userId === 1}/>
+                ))}
             </div>
         </ChatBoxTop>
 
         <ChatBoxBottom>
-            <ChatMessageInput></ChatMessageInput>
-            <ChatSubmitButton>전송</ChatSubmitButton>
+            <ChatMessageInput value={text} onChange={(e) => setText(e.target.value)}></ChatMessageInput>
+            <ChatSubmitButton onClick={() => send()}>전송</ChatSubmitButton>
             <label for="file">
                 <FileUpload><AttachFileOutlinedIcon style={{"fontSize":"22px"}} /></FileUpload>
             </label>
--- src/redux/slices/chatSlice.js
+++ src/redux/slices/chatSlice.js
@@ -1,6 +1,35 @@
+import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import axios from "axios";
 
 const initialState = {
     messages: [],
     chatList: [], 
     isLoading: true
 }
+
+export const getChatMessage = createAsyncThunk("GET_CHAT_MESSAGE", async (payload, thunkAPI) => {
+
+    const config = {
+        headers: {
+            "X-ACCESS-TOKEN": window.localStorage.getItem("token")
+        }
+    }
+    try{
+        const response = await axios.get(`/api/chat-rooms/${payload.roomId}/messages`, config);
+        return response;
+    }catch(e){
+        return thunkAPI.rejectWithValue(e.response.data);
+    }
+})
+
+const chatSlice = createSlice({
+    name:"chat",
+    initialState,
+    extraReducers: (builder) => {
+        builder.addCase(getChatMessage.fulfilled, (state, action) => {
+            state.messages = action.payload.data;
+        });
+    }
+});
+
+export default chatSlice.reducer;
