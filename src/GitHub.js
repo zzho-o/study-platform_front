27d63ed444971aa99a87ff3b8ac58c63e18ac7d7
zzho-o <rkdwjsgh0@naver.com>
2022-09-11 16:08:44 +0900

    'feat'

--- src/GitHub.js
+++ src/GitHub.js
@@ -1,5 +1,5 @@
 const REST_API_KEY = "399d0fa02a0200f5b814";
-const REDIRECT_URI = "http://54.180.148.170:8000//Redirect";
+const REDIRECT_URI = "http://54.180.148.170:3000//Redirect";
 const GITHUB_AUTH_URL = `https://github.com/login/oauth/authorize?client_id=${REST_API_KEY}&scope=repo:status read:repo_hook user:email&redirect_uri=${REDIRECT_URI}`;
 
 export { GITHUB_AUTH_URL };
--- src/Kakao.js
+++ src/Kakao.js
@@ -1,5 +1,5 @@
 const REST_API_KEY = "3bfb036a57b6100db7aff9142cc1c744";
-const REDIRECT_URI = "http://54.180.148.170:8000/Redirect";
+const REDIRECT_URI = "http://54.180.148.170:3000/Redirect";
 const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
 export { KAKAO_AUTH_URL };
