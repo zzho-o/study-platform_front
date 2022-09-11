37fef051456b8d9772785883280c19ce460eef6c
zzho-o <rkdwjsgh0@naver.com>
2022-09-11 13:02:51 +0900

    'feat'

--- src/Redirect.js
+++ src/Redirect.js
@@ -1,17 +1,31 @@
 import * as React from "react";
+import { useNavigate } from "react-router-dom";
 const axios = require("axios");
 
 function Redirect() {
   const code = new URL(window.location.href);
   const code_params = code.searchParams.get("code");
+  const navigate = useNavigate();
+  let data;
+  // React.useEffect(async () => {
+  //     await axios.post('https://jadu-study-flatform.shop/api/oauth/kakao', {
+  //     headers: {
+  //       token: code_params,
+  //     },
+  //   });
+  // })
   const get_info = async () => {
     try {
-      return await axios.post(
-        "https://jadu-study-flatform.shop/api/oauth/kakao",
-        {
-          code: code_params,
-        }
-      );
+      return await axios
+        .post("https://jadu-study-flatform.shop/api/oauth/kakao", {
+          headers: {
+            token: code_params,
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          navigate("/");
+        });
     } catch (err) {
       console.log(err);
     }
