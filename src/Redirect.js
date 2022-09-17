ab6c4eeaf9d67a36d4b291a0a24d321d998f4498
zzho-o <rkdwjsgh0@naver.com>
2022-09-17 22:51:09 +0900

    '소셜로그인api'

--- src/Redirect.js
+++ src/Redirect.js
@@ -6,18 +6,11 @@ function Redirect() {
   const code = new URL(window.location.href);
   const code_params = code.searchParams.get("code");
   const navigate = useNavigate();
-  // React.useEffect(async () => {
-  //     await axios.post('https://jadu-study-flatform.shop/api/oauth/kakao', {
-  //     headers: {
-  //       token: code_params,
-  //     },
-  //   });
-  // })
-  const get_info = async () => {
+  React.useEffect(async () => {
     try {
       const data = await axios
         .post("https://jadu-study-flatform.shop/api/oauth/kakao", {
-          headers: {
+          body: {
             token: `${code_params}`,
           },
         })
@@ -29,7 +22,7 @@ function Redirect() {
       navigate("/");
       alert(`로그인 실패!:${err}`);
     }
-  };
+  }, []);
   get_info();
   return <div></div>;
 }
